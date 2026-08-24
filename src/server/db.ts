ec72d88521bf6f39ffeac91069b94127c0c05980
balasokhot365-lab <balasokhot365@gmail.com>
2026-08-25 01:35:05 +0530

    firebase changes

--- src/server/db.ts
+++ src/server/db.ts
@@ -16,8 +16,8 @@ function getServiceAccount() {
     }
   }
 
-  const projectId = process.env.FIREBASE_PROJECT_ID || "ekdant-mandel-2026";
-  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL || "firebase-adminsdk-fbsvc@ekdant-mandel-2026.iam.gserviceaccount.com";
+  const projectId = process.env.FIREBASE_PROJECT_ID || "ganesh-mitra-mandal";
+  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL || "firebase-adminsdk-fbsvc@ganesh-mitra-mandal.iam.gserviceaccount.com";
   let privateKey = process.env.FIREBASE_PRIVATE_KEY;
 
   if (privateKey) {
@@ -25,34 +25,7 @@ function getServiceAccount() {
     privateKey = privateKey.replace(/\\n/g, "\n");
   } else {
     // Default fallback service account key
-    privateKey = `-----BEGIN PRIVATE KEY-----
-MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDD4IHs8IVQIJyD
-f0cNDiAUiMewg+0V5tK5+n9tmOPlwTIf+sh9jhd7aYHAB7g8qqaQsrDLjmYKSZMB
-yDxee4sBIhNynFknggEDEx5XJM1PPN6Fc18i/v+Df4giRZ3+cJKkOySDnGjrkEad
-09k6PUdLyfI6t1v0zYbLf/OwrLdp116VOLImpBaLsEjJO4LEcmiWz03EqtBmbFLD
-YPjjQ+crdshbtd7waj5W5bZKoO259Mo2YL2dONWC98YC9gv5HtWCeaEfTwUBPalq
-q3d4NZTzg5T9H76bUlRFQR1R2WiCA0W+a6Bot9Nui9HiiPzPuzHJYvQDaS7jSJVu
-nAiTU+wjAgMBAAECggEAQ71aIV9isz278uvq3fRzGrbc3EobjVX4rVGYjQXxVo0U
-MSQQ/v7LVyY7NXZV/EEF/oeVhaf22PrtyJF/cytlfJxUvm+ck4GtqOsNNJTpHcyO
-pfcqwRcPtWG+6h9KTuqOhBZh/9PxIvoPudhASRhVFjw+NZkgx43mXm0fEQwCGCtD
-7MUAD9RJ9Eg9vJZyqd8d7Rr0Lgi1WjU6TmvM+FuM14iwzNbTmve4UBQh+h+3a29o
-XYOdbgpDsddyvVf3SpjhNamX5NeRRI88gh67EIz0AO2AzDgQhphjZpzHD4KSKhEv
-89pnq30zOajhWGEtsEHBWgY8PSmPATvYq1h+ppEVsQKBgQD49UqwR4EJS6znEiA4
-zm6EQOF38E2Vjn0Z3CXbqPjeKrntuirVjBDWVMjV1JTBjaG4vqTacha41cKSZucS
-N/453Ma/JzCnVSpmXUdmM4keYO9PUGzW1OI/RIy8yeiNgto1eBY5W9h/GeWrgyfH
-PNPoLRRTqSyzfWlL8DjRX9ytMQKBgQDJatq75OehUtn002p6CQ+28vTLqIpCavl0
-n/rHKBFtao5wM6I+SLkhQeW7px98of0OvDMy/WnrKEGOwL4zvkSLvMy20/OrN8kV
-6aUAX4Q6pEAdYGD2K0hFrHrdmXgxSlwfj/USOXv2kK6Ky5TwxRayqf71WCeSnhiC
-eFwWoXHJkwKBgHMLfyk40cNG6i6ZrLQziNSq2sY/EMs03lCt7/yy99ZKrkLSDetC
-36D76xcNNnS/C2XBC5M6t49QkKpwzQUZROzePiWlaZFaBM7q+gybchjMuKnRQ4p/
-M9ICznqLX2DNgCLHTJXTAJezkvOQFzi1vkMDWepTOinzoU4LQ2ZVwzdxAoGAKh6/
-WTWYPsM9sxYXGSgV8jNCf+hh6VvHiNz/q3A0nMYAvXP+xLmehVbrfN+JFR4m9lLi
-/hHCeZu0ge7Kl7V9Th+QS9dtCGwlEAEJMVfIJh89DSeffzsz5OYZ14eFfAFJ3IvV
-OxdsVfaRmZLTNbpxcbFdEN97fwyyoW4cC9t9GSMCgYEAmmoK/Oi/N0KiVRK5aguV
-7FwW6zp6ZIwuR2OHBk6wHfTLfVoWYVjYQl/J8Pxt3wgFbYO6HKYXYQ315nBYcR+w
-C4+K2I4yaHjpdlrruUxroWj4JlZz4kgaS3pLjZlaew1+a6WNWXnEe4f5Ob+5eb4G
-yPbjgBlF6AVG+21Hy76OSIA=
------END PRIVATE KEY-----`;
+    privateKey = `-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDR+VK6xzmKnLj5\nTvaiaRPIsDRcnnztDp6yQdQ6kPJU4gl2cjVttb8zf9RUiVwnPjM5wqr4RI2+4lXl\nZu8ZB7u2F4rLj0pV0Wt9viXM3aTAbn3VDpg0zIZ2gBXEz8wA142jDuY7Mz7GJ0jT\n3dGM6YiMzrV8nkgNdvGL4VTFcMF0AUf0nOQsUykDHiiymiDSshhIsr5tTIKSS8ck\nZqpP9u6BsYGU5WFa5GamhNNoggBX4WzgnDT0N/GC3kqqyYEGjRucjbDKoHNv66EY\nhL5QNYWvCye08RhllPH+ZwJpYvhpQdz+RwVlG/WR4O/upPAXyLPJTQ38s+NMi9Yi\nTUS0yUJDAgMBAAECggEAN3HwNAzud1kqCP8eZkV76WcJ9JkPIOPW6Pg0C+EBdIOZ\nfxLqY9V1ryomjcoVsPEeQrbSQauMiGaoyuIy6beejBdMWbM1LFMX4MTIo3+1hPDI\nPm3TqEGHkhwBY46Bbol8x51kEybVgSqXcwakUmLKYXAmAUmkehW587dlFJvRIYnI\nmzcnlJY2Kfz0pMZSa6c0Yz4sZv0NXSSMbv+XZj6B+4sDcQToXSMG+9+806pWl7XC\n7Z0ViQG3D5b0u6H/hFQK7KfoIdFXT0S5jQaXNLAwSkoFBibDtgPd2e+eC14/4TbM\nURbbkyJC2ZeujN6I9xn25b9qqJf/JSK0+3FbQ8J3mQKBgQDrGm7Qk6Cu9T5BCX1G\ndqqTm6JqJRLyzCmRK5gIWSbgW920XO4/pLY58tHRjcVV0mAQn/jfr4ZU6DqJ5vTv\ncdunPoWxMvcE8ItwZQ7NvNsrL+Ttg258O03EbXQZ6jwhA7FcrwnWcEeOuSizpbej\nik/pYFvmpnY5xRGu0eeCW2oNaQKBgQDkoxgttb0nUkw9SVBYLnzAh3s2uxIj95pI\ny5ePVbQlGxDFajJ5uMr+Ro+LpSeDE0aqylDvF4MnQvgCUZpXXZiIM6be7RtE58yF\nnc8fi+2tWj5fSqs2Hnb2GG5N3acBD8yr1QgU4kRVRlAimJnHwQTfcybj+/jGcK0M\nDkZ2J/egywKBgBPyEiSv4Zn2RGnQHEi2GJKdSZInwwvGNmCSijtrFNlD8fMzTyMn\nHi6cYyCcHnnBd1TxvXN8uLDLYuwAJBeMWU4/B6iY4kwQ/EN0z2S0+QMY9RsRY3Y2\nscpQXuEz9hX28j0ivHrs26VIICPdk07UlSL/gcx3ouAT0AQ82p88kirBAoGBAKLk\noAFRDibJt4+igB+TX0Y8QEjKFy6Q5dcfVifEw8f79ILyGmwmgFmRJAw16mS0gxCD\nfA4cDlEWoWPlh7Te9iFzy1vgSiFZlJlNyZeMNhXfybhSlpDEO7UxysSnOqjC/Osf\np9ZZLp4irk03s7hhiHs6q2aayq9275M7SHA+IWrpAoGBAMoHDLK0YerzDNqn/xT5\ntggIUvbX4TDKRW66Gr+atGFkoyDcUW4/WbWFzSHwV/CQ6oECxghU3kD7WDO0ft5D\nYCzLzRobMYbOezgSRrpklpRHvOUQMuYClf205LpL27T8JBqwy6KFwliXr1Ki5abg\nHcUAfsbLgYAD5cDoZ1lgDuQ6\n-----END PRIVATE KEY-----\n`;
   }
 
   return {
