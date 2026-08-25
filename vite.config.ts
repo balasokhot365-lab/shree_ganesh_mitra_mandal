27e074ae3ac93c4e9a2bd4a9ddbe63eff8207bb2
balasokhot365-lab <balasokhot365@gmail.com>
2026-08-25 11:38:25 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,24 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
 
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
     },
-  },
 
-  server: {
-    host: '0.0.0.0',
-    hmr: process.env.DISABLE_HMR !== 'true',
-    watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    server: {
+      host: '0.0.0.0',
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
 
-    allowedHosts: [
-      'shree-ganesh-mitra-mandal1.onrender.com',
-    ],
-  },
+      allowedHosts: [
+        'shree-ganesh-mitra-mandal-1.onrender.com',
+      ],
+    },
+  };
 });
